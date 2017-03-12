c189ea7a9a65ad2039980de13a110291f208b9dc
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-03-12 16:30:22 +0100

    Add email module

---
+++ app/email.py
@@ -0,0 +1,21 @@
+from threading import Thread
+from flask import current_app, render_template
+from flask_mail import Message
+from . import mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_mail(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(
+        subject=app.config['WEATHER_APP_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+        recipients=[to],
+        sender=app.config['WEATHER_APP_MAIL_SENDER'])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
