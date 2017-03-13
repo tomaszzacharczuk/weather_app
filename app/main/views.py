5f66a0b858fe0ea744115b65545851c61bf812bc
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-03-13 16:36:27 +0100

    Implement dashboard theme

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,8 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
