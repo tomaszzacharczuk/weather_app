ced507558a85e82f750429d40aa8262c77cdf5be
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-03-15 17:11:11 +0100

    Add email send fallback
    If confirmation email cannot be send, user is confirmed automatically.
    I assume that senior dev won't have SSL configured locally so he (or she)
    won't set his email and password in environment variables for security reasons.
    To keep app usable, I confirm user without email but password reset
    and email change will not work.

--- app/email.py
+++ app/email.py
@@ -2,11 +2,27 @@ from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
+from .models import User
+from app import db
 
 
 def send_async_email(app, msg):
     with app.app_context():
-        mail.send(msg)
+        try:
+            mail.send(msg)
+        except:
+            """If confirmation email cannot be send, user is confirmed automatically.
+            I assume that senior dev won't have SSL configured locally so he (or she)
+            won't set his email and password in environment variables for security reasons.
+            To keep app usable, I confirm user without email but password reset
+            and email change will not work."""
+            recipients = msg.recipients
+            for email in recipients:
+                user = User.query.filter_by(email=email).first()
+                user.confirmed = True
+                db.session.add(user)
+                db.session.commit()
+                print('User ' + user.email + ' automatically confirmed.')
 
 
 def send_email(to, subject, template, **kwargs):
